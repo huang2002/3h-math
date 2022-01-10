58768af7bbabfc90d2c2d4063d3a9b04509bb5de
hhh <1340545944@qq.com>
2022-01-10 20:20:11 +0800

    improve display

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 fs.readdirSync(__dirname).forEach(file => {
     if (file.endsWith('.test.js')) {
-        console.log(`### Running ${file}\n`);
+        console.log(`>>> Running ${file}\n`);
         require(`./${file}`);
     }
 });
